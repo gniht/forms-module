90bd0b985e23594fd03a3f54fd632d89a96b9965
Jason Thomas <jasonthomasjat@gmail.com>
2021-02-24 15:59:23 -0800

    more styling tweaks

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-//import country_list from "./countries";
-let country_list = ["Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua &amp; Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia &amp; Herzegovina","Botswana","Brazil","British Virgin Islands","Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Cape Verde","Cayman Islands","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica","Cote D Ivoire","Croatia","Cruise Ship","Cuba","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea","Estonia","Ethiopia","Falkland Islands","Faroe Islands","Fiji","Finland","France","French Polynesia","French West Indies","Gabon","Gambia","Georgia","Germany","Ghana","Gibraltar","Greece","Greenland","Grenada","Guam","Guatemala","Guernsey","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hong Kong","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Isle of Man","Israel","Italy","Jamaica","Japan","Jersey","Jordan","Kazakhstan","Kenya","Kuwait","Kyrgyz Republic","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macau","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Mauritania","Mauritius","Mexico","Moldova","Monaco","Mongolia","Montenegro","Montserrat","Morocco","Mozambique","Namibia","Nepal","Netherlands","Netherlands Antilles","New Caledonia","New Zealand","Nicaragua","Niger","Nigeria","Norway","Oman","Pakistan","Palestine","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Puerto Rico","Qatar","Reunion","Romania","Russia","Rwanda","Saint Pierre &amp; Miquelon","Samoa","San Marino","Satellite","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","South Africa","South Korea","Spain","Sri Lanka","St Kitts &amp; Nevis","St Lucia","St Vincent","St. Lucia","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Timor L'Este","Togo","Tonga","Trinidad &amp; Tobago","Tunisia","Turkey","Turkmenistan","Turks &amp; Caicos","Uganda","Ukraine","United Arab Emirates","United Kingdom","USA","Uruguay","Uzbekistan","Venezuela","Vietnam","Virgin Islands (US)","Yemen","Zambia","Zimbabwe"];
+import country_list from "./countries.js";
+
 
 const countries = country_list;
 console.log(countries)
 const myForm = document.createElement('form');
-myForm.classList.add('left-card')
+myForm.classList.add('split-left');
 const formList = document.createElement('ul');
 
 myForm.append(formList);
@@ -46,8 +46,6 @@ submit.addEventListener('click', e => {
 });
 
 
-
-
 /* this function is probably not saving much time, but sometimes 
 using JS to make html elements just feels really repetitive */ 
 function mkLabeledInputLi (name, type = 'text', txt = '', req = false) {
@@ -71,6 +69,9 @@ function mkLabeledInputLi (name, type = 'text', txt = '', req = false) {
 }
 
 function appendCountries(countrySelectElement, countriesArr) {
+  let chooseCountry = document.createElement('option');
+  chooseCountry.innerText = '< Select Country >';
+  countrySelectElement.append(chooseCountry);
   for(let i = 0; i < countriesArr.length; i++) {
     let opt = document.createElement('option');
     opt.innerText = `${countriesArr[i]}`;
