7f925253eeb119e07d36c6fe66a0b13afca9cd3e
Jason Thomas <jasonthomasjat@gmail.com>
2021-02-23 18:23:25 -0800

    event listener to grab values from form

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const formList = document.createElement('ul');
 myForm.append(formList);
 document.body.append(myForm);
 
-const nameLi = mkLabeledInputLi('name', 'text', 'Name:');
-const emailLi = mkLabeledInputLi('email', 'email', 'E-mail:');
+const nameLi = mkLabeledInputLi('name', 'text', 'Name:', true);
+const emailLi = mkLabeledInputLi('email', 'email', 'E-mail:', true);
 const countryLi = mkLabeledInputLi('country', 'text', 'Country:');
 const zipcodeLi = mkLabeledInputLi('zipcode', 'number', 'Zip code:');
-const pwLi = mkLabeledInputLi('pwLi', 'password', 'Password:');
-const pwConfirmLi = mkLabeledInputLi('pwConfirmLi', 'password', 'Confirm PW:');
+const pwLi = mkLabeledInputLi('pwLi', 'password', 'Password:', true);
+const pwConfirmLi = mkLabeledInputLi('pwConfirmLi', 'password', 'Confirm PW:', true);
 const submit = document.createElement('button');
 submit.type = 'submit';
 submit.innerText = 'Submit';
@@ -25,12 +25,22 @@ formList.append(
   submit
 );
 
+submit.addEventListener('click', e => {
+  e.preventDefault();
+  let name = nameLi.children[1].value;
+  let zip = zipcodeLi.children[1].value;
+  let pw = pwLi.children[1].value;
+  let pwConfirm = pwConfirmLi.children[1].value;
+});
+
+
+
 
 
 
 /* this function is probably not saving much time, but sometimes 
 using JS to make html elements just feels really repetitive */ 
-function mkLabeledInputLi (name, type = 'text', txt = '') {
+function mkLabeledInputLi (name, type = 'text', txt = '', req = false) {
   let listItem = document.createElement('li');
 
   let lbl = document.createElement('label');
@@ -38,6 +48,9 @@ function mkLabeledInputLi (name, type = 'text', txt = '') {
   lbl.innerText = txt || name;
 
   let input = document.createElement('input');
+  if (req) {
+    input.required = true;
+  }
   input.type = type;
   input.id = name;
   input.name = name;
